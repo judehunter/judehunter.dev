68d1d83ba58a81a3a0deb6b726f417d464e974fd
judehunter <matisowagm@gmail.com>
2022-09-19 22:21:01 +0200

    fix: Double page_view gtaag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,29 @@
+import Script from 'next/script';
 import {GlobalStyles} from 'twin.macro';
 import '../misc/global.css';
-import {useGAPageView} from '../misc/gtag';
 
 const App = ({Component, pageProps}) => {
-  useGAPageView();
+  // useGAPageView();
   return (
     <>
       <GlobalStyles />
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-VF7DM82296`} />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VF7DM82296');
+          `,
+            }}
+          />
+        </>
+      )}
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -19,23 +19,6 @@ export default class MyDocument extends Doc {
             rel="stylesheet"
           /> */}
           <style data-emotion-css={this.props.ids.join(' ')} dangerouslySetInnerHTML={{__html: this.props.css}} />
-          {process.env.NODE_ENV === 'production' && (
-            <>
-              <script async src="https://www.googletagmanager.com/gtag/js?id=G-VF7DM82296" />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-VF7DM82296', {
-                      page_path: window.location.pathname,
-                    });
-                  `,
-                }}
-              />
-            </>
-          )}
         </Head>
         <body>
           <Main />
