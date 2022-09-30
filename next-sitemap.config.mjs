958cb132a8b55b3f2fcf7ae4de0c364d63fc9493
judehunter <matisowagm@gmail.com>
2022-09-30 18:03:08 +0200

    stuffff

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,52 @@
-import {readFile} from 'fs/promises';
+import {readdir, readFile} from 'fs/promises';
 import {serialize} from 'next-mdx-remote/serialize';
 import path from 'path';
+import { visit } from 'unist-util-visit';
+
+const serverSerializeMDX = (text) => {
+  return serialize(text, {
+    parseFrontmatter: true,
+    mdxOptions: {
+      remarkPlugins: [
+        () => (tree) => {
+          visit(tree, (node) => {
+            if (node.type === 'text') {
+              node.value = (node.value).replace(/---/g, '—').replace(/--/g, '–').replace(/'/g, '’');
+            }
+          });
+        },
+      ],
+    },
+  });
+};
+
+export const serverSerializeAllPosts = async () => {
+  const filesOrDirs = await readdir(path.join('content', 'posts/'));
+
+  const posts = await Promise.all(
+    filesOrDirs.map(async (fileOrDir) => {
+      const isDir = !fileOrDir.endsWith('.mdx');
+
+      const filePath = isDir
+        ? path.join('content', 'posts/', fileOrDir, 'index.mdx')
+        : path.join('content', 'posts/', fileOrDir);
+
+      const file = await readFile(filePath, 'utf-8');
+
+      const source = await serverSerializeMDX(file);
+
+      return {
+        source,
+        slug: fileOrDir.replace('.mdx', ''),
+        url: '/blog/' + fileOrDir.replace('.mdx', ''),
+      };
+    }),
+  );
+
+  return posts;
+};
+
+const posts = await serverSerializeAllPosts();
 
 /** @type {import('next-sitemap').IConfig} */
 export default {
@@ -29,14 +75,12 @@ export default {
       };
 
     if (p.startsWith('/blog/')) {
-      const mdxData = await serialize(await readFile(path.join('content', 'posts/', p.slice(6) + '.mdx'), 'utf-8'), {
-        parseFrontmatter: true,
-      });
+      const {source} = posts.find(x => x.url === p);
       return {
         loc: p,
         changefreq: config.changefreq,
         priority: 0.8,
-        lastmod: mdxData.frontmatter.updateDate,
+        lastmod: source.frontmatter.updateDate,
         alternateRefs: config.alternateRefs ?? [],
       };
     }
