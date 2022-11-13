c7b03605a7ba0cff0d20fbc00ee8c14f87343209
judehunter <matisowagm@gmail.com>
2022-11-13 19:31:00 +0100

    wip

--- content/posts/assembly-interpreter-in-typescripts-type-system/test.ts
+++ content/posts/assembly-interpreter-in-typescripts-type-system/test.ts
@@ -16,4 +16,21 @@ type Tail<T> = T extends `${string}${infer R}`
     : R
   : never;
 
+type FindPredicate<T, PC> = T extends {idx: PC} ? T : never;
+
+type Find<Instrs, PC> = {
+  [K in keyof Instrs as K extends number ? K : never]: FindPredicate<
+    Instrs[K],
+    PC
+  >;
+}[any];
+
+type Clean<Arr> = {
+  [K in keyof Arr as K extends number ? K : never]: Arr[K];
+}[any];
+
+type CleanFind<Instrs, PC> = Clean<Find<Instrs, PC>>;
+
+type Test = Find<[{idx: '0'; a: 'a'}, {idx: '1'; b: 'b'}], '1'>;
+
 export {};
